d4a5c68ede49e27ebde8d82fa9173754fb7d888d
Philsource <56137055+Philsource@users.noreply.github.com>
2022-04-11 08:34:59 +0200

    Started Loop

--- Loop.java
+++ Loop.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Loop {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q=in.nextInt();
+        for(int i = 0; i < q; ++i) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            // here it goes
+        }
+        in.close();
+    }
+
 }
