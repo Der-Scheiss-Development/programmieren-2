b5cc32817d0364eb37f308b597a62390276ec91d
Philsource <56137055+Philsource@users.noreply.github.com>
2022-04-21 12:12:21 +0200

    Added output to test with the added reference in Person.java

--- programming2/src/edu/htwk/fdit/prog2/uebung3/Main.java
+++ programming2/src/edu/htwk/fdit/prog2/uebung3/Main.java
@@ -9,9 +9,19 @@ public class Main {
 
     public static void main(String[] args) {
         LocalDate test = LocalDate.of(1904,12,12);
-        Person Heinz = new Person("Test","Vorname",19,12,1999);
-        System.out.println(Heinz.geburtsJahr);
         Address Atest = new Address("Teststraße", "12", "12345", "Testort", "Testland");
+        BankAccount Btest = new BankAccount("12384", "12345", "KTO123");
+        Person Heinz = new Person("Test","Vorname",19,12,1999, Atest, Btest);
+        System.out.println(Heinz.getGeburtstag());
+
+
         System.out.println(Atest.getAddress(Atest.strasse, Atest.hausnummer, Atest.plz, Atest.ort, Atest.land));
+
+
+        System.out.println(Btest.getKto() + " " + Btest.getBlz());
+        System.out.println(Btest.iban);
+        System.out.println();
+        System.out.println(Heinz.bankAccount.getIban());
+
     }
 }
