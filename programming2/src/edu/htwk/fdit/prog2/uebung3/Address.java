6540e116e77dc38c911a73d50e0e5d2d62250e4a
Philsource <56137055+Philsource@users.noreply.github.com>
2022-04-21 10:23:41 +0200

    Added class Address

---
+++ programming2/src/edu/htwk/fdit/prog2/uebung3/Address.java
@@ -0,0 +1,14 @@
+package edu.htwk.fdit.prog2.uebung3;
+
+public class Address {
+    String strasse, hausnummer, plz, ort, land;
+
+    public Address(String strasse, String hausnummer, String plz, String ort, String land) {
+        this.strasse = strasse;
+        this.hausnummer = hausnummer;
+        this.plz = plz;
+        this.ort = ort;
+        this.land = land;
+    }
+
+}
