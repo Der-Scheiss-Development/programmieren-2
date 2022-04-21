24e9564e1f7864d7a244ff8c3dc7132ccbf32672
Philsource <56137055+Philsource@users.noreply.github.com>
2022-04-21 10:23:30 +0200

    Began with class Person

--- programming2/src/edu/htwk/fdit/prog2/uebung3/Person.java
+++ programming2/src/edu/htwk/fdit/prog2/uebung3/Person.java
@@ -1,17 +1,17 @@
 package edu.htwk.fdit.prog2.uebung3;
+import java.time.*;
 
 public class Person {
-    String name, vorname, geburtsTag, geburtsMonat, geburtsJahr, strasse, hausnummer, plz, ort, land, iban, bic, blz, kto;
+    String nachname, vorname, geburtsTag, geburtsMonat, geburtsJahr;
 
-    public Person() {
-        // IT`S EMPTY IN HERE
+    public Person(String nachname, String vorname, String geburtsTag, String geburtsMonat, String geburtsJahr) {
+        this.nachname = nachname;
+        this.vorname = vorname;
+        this.geburtsTag = geburtsTag;
+        this.geburtsMonat = geburtsMonat;
+        this.geburtsJahr = geburtsJahr;
+
     }
 
-    public int hashCode() {
-        final int prime = 31; // multiplication with 31 is super fast by shifting
-        int result = 1;
-        result = prime * result + (( name == null) ? 0 : name.hashCode()); // für Strings, -> oder zusammengesetzte Datentypen
-        // I NEED CODE
-        return result;
-    }
 }
+
